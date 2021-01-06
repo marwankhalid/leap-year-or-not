0d4a6cffcebc16962268a098b1d6c7a047bfcc33
Marwan Khalid <33214208+marwankhalid@users.noreply.github.com>
2021-01-06 22:37:37 +0500

    Done

--- leap Year or Not/main.cpp
+++ leap Year or Not/main.cpp
@@ -6,9 +6,41 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    while (true) {
+        int year = 0;
+        cout<<"Enter Any Year \n";
+         
+        if (year == 0) {
+            return 0;
+            break;
+        }
+         bool flag = false;
+         if(year % 4 == 0){
+           flag = true;
+           if (year % 100 == 0){
+             if ((year % 400) == 0){
+                 //cout<<year<<" is a leap year";
+             }else{
+               //cout<<year<<" is not a leap year";
+             }
+           }else{
+             //cout<<year<<" is a leap year";
+           }
+         }else{
+           //cout<<year<<" is not a leap year";
+           flag = false;
+         }
+         
+         if (flag){
+           cout<<"Yes\n";
+         }else{
+           cout<<"No\n";
+         }
+        
+    }
+    
 }
